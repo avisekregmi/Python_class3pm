b231d5a6d91bf18cd93566512891cc8234afe4bb
avisekregmi <avisek.regmi@gmail.com>
2022-09-16 12:36:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("This is main file:")
+a = 99
+b = 100
+c = a * b
+print("The multiplication is {}".format(c))
+
---
+++ src/app.py
@@ -0,0 +1,4 @@
+print("This is app")
+print("Hi")
+# C:\Users\aregmi\Desktop\pythonlab2\src\app.py
+# src\app.py
